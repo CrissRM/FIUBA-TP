ce6be5144dcfd02f825bde5d065d80c982bb2e28
CrissRM <criss.rol.10@gmail.com>
2022-05-23 10:58:45 -0300

    Ingreso de nombres formateados

--- helpers/ingreso_de_jugadores.py
+++ helpers/ingreso_de_jugadores.py
@@ -1,5 +1,7 @@
-import os
 from initialSetting.datos_iniciales import condiciones_iniciales
+from helpers.formatear_nombre import formatear_nombre
+import os
+
 
 lista_cantidad = condiciones_iniciales()["cantidad_jugadores"]
 MAXIMO = len(lista_cantidad)
@@ -13,7 +15,7 @@ def ingreso_de_jugadores():
     try:
       cantidad_jugadores = int(input("\nIngrese la cantidad de jugadores: "))
     except ValueError:
-      print("\x1b[31m >>> ALERTA!!! Debe ingresar un numero\x1b[0m")
+      print("\n\x1b[31m >>> ALERTA!!! Debe ingresar un numero\x1b[0m")
     else:
       if cantidad_jugadores not in lista_cantidad:
         print(f"\n\x1b[33mPor el momento, el juego esta implementado para un maximo de dos {MAXIMO} jugadores\x1b[0m")
@@ -23,14 +25,16 @@ def ingreso_de_jugadores():
   lista_jugadores = []
   for i in range(cantidad_jugadores):
     jugador = input(f"\nPor favor, ingrese el nombre del {i+1}° jugador: ")
-    while not (jugador.isalpha()):
-      print("\x1b[31m>>>> ALERTA!!! Solo se aceptan caracteres alfabeticos...\x1b[0m")
+    check,jugador = formatear_nombre(jugador)
+    while not (check):
+      print("\n\x1b[31m>>>> ALERTA!!! Solo se aceptan caracteres alfabeticos...\x1b[0m")
       jugador = input("Por favor, ingrese un nombre valido: ")
+      check,jugador = formatear_nombre(jugador)
     
     while jugador in lista_jugadores:
       print("\n\x1b[31m>>> ALERTA Debe ingresar un nombre diferente al anterior\x1b[0m")
       jugador = input("Por favor, ingrese otro nombre: ")
     
-    lista_jugadores.append(jugador.capitalize())
+    lista_jugadores.append(jugador)
   return lista_jugadores
 
